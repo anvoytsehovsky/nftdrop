9b9a3b46740ff839b78b10871b913ebd73e28274
Andrew Voytsehovsky <anvoytsehovsky@gmail.com>
2022-08-08 21:12:58 +0300

    Building the Left Section of the NFT Drop Page

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
 const NFTDropPage = () => {
-  return <div>Drop</div>;
+  return (
+    <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
+      {/* Left */}
+      <div className="lg:col-span-4 bg-gradient-to-br from-cyan-800 to-rose-500">
+        <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
+          <div className="bg-gradient-to-br from-yellow-400 to-purple-600 p-2 rounded-xl">
+            <img
+              className="w-44 rounded-xl object-cover lg:h-96 lg:w-72"
+              src="https://links.papareact.com/8sg"
+              alt=""
+            />
+          </div>
+          <div className="text-center p-5 space-y-2">
+            <h1 className="text-4xl font-bold text-white">PAPAFAM Apes</h1>
+            <h2 className="text-xl text-gray-300">
+              A colection of PAPAFAM Apes who live & breathe React!
+            </h2>
+          </div>
+        </div>
+      </div>
+      {/* Right */}
+      <div className=""></div>
+    </div>
+  );
 };
 
 export default NFTDropPage;
