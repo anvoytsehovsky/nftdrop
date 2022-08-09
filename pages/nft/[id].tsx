42dea4b46fcd452f82fdc6cc18c51d592e3fd46e
Andrew Voytsehovsky <anvoytsehovsky@gmail.com>
2022-08-09 11:33:51 +0300

    Adding Wallet Address Message

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -49,6 +49,13 @@ const NFTDropPage = () => {
         </header>
 
         <hr className="my-2 border" />
+        {address && (
+          <p className="text-center text-sm text-rose-400">
+            You're logged in with wallet {address.substring(0, 5)}...
+            {address.substring(address.length - 5)}
+          </p>
+        )}
+
         {/* Content */}
         <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:space-y-0 lg:justify-center">
           <img
